e1ca6f781bee4ebaf4af5a6c37bda5c5529d82f8
markmnl <markmnl@outlook.com>
2025-01-25 12:18:37 +0800

    pop only if is blowingup

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -67,6 +67,7 @@ public class Player : MonoBehaviour
                 activeBubble.transform.position = transform.position;
             } else if (Input.GetKeyUp(KeyCode.Space)) {
                 Physics2D.IgnoreCollision(activeBubble.GetComponent<Collider2D>(), this.GetComponent<Collider2D>(), false);
+                activeBubble.IsBlowingUp = false;
                 activeBubble = null;
             }
         }
