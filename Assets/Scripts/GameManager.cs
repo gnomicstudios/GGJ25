9430849c4655a6c2e58872e21c70b0f9f4d7bbc9
Aranda Morrison <aranda@gnomicstudios.com>
2025-01-25 11:24:05 +0800

    First pass of UI - HudController and GameController (not hooked up yet)

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int bubbles = 20;
+    public int lives = 5;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/HudController.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using TMPro;
+public class HudController : MonoBehaviour
+{
+    public TextMeshProUGUI bubblesText;
+    public TextMeshProUGUI livesText;
+
+
+    private GameManager game;
+    private int bubbles = 0;
+    private int lives = 0;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        game = FindFirstObjectByType<GameManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (game.bubbles != bubbles) {
+            bubbles = game.bubbles;
+            bubblesText.text = bubbles.ToString();
+        }
+        if (game.lives != lives) {
+            lives = game.lives;
+            livesText.text = lives.ToString();
+        }
+    }
+}
